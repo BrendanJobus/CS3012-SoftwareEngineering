2113bedbc688d857664b741b4b709bb0ee5192dd
Jarota <32527884+Jarota@users.noreply.github.com>
2018-09-25 16:16:35 +0100

    added more tests and found bug

--- LCA assignment/LCAtest.py
+++ LCA assignment/LCAtest.py
@@ -28,6 +28,18 @@ class LCATestClass(unittest.TestCase):
 		#	  /	  \	   /
 		#	0	   4 7
 		self.assertEqual(g.lca([0, 4]), 2)
+		self.assertEqual(g.lca([0, 4, 7]), 5)
+
+	def testSingleNode(self):
+		g = BinaryTree()
+		g.add(5)
+		g.add(2)
+		g.add(4)
+		g.add(8)
+		g.add(7)
+		g.add(0)
+		self.assertEqual(g.lca([5]), 5)
+		self.assertEqual(g.lca([7, 8]), 8)
 
 if __name__ == "__main__":
 	unittest.main()
