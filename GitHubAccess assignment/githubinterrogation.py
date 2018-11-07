7c447e536b2a8ec5afa13d5c0a220d3b893d8230
Jarota <32527884+Jarota@users.noreply.github.com>
2018-11-07 21:45:49 +0000

    can now continuously trawl through users and repos

--- GitHubAccess assignment/githubinterrogation.py
+++ GitHubAccess assignment/githubinterrogation.py
@@ -7,7 +7,53 @@ pwd = getpass.getpass()
 g = Github(usr, pwd)
 
 print("")
-print("{} has the following repos:".format(usr))
+print("You have the following repos:")
 
-for repo in g.get_user().get_repos():
+repos = g.get_user().get_repos()
+for repo in repos:
 	print(repo.name)
+
+while True:
+	print("")
+	repo_name = input("Pick a repo: ")
+
+	if repo_name == "quit":
+		break
+
+	match = False
+	selected_repo = None
+	for repo in repos:
+		if repo.name == repo_name:
+			selected_repo = repo
+			match = True
+			break
+
+	if match == False:
+		print("That is not the name of any repo.")
+
+	else:
+		contributors = selected_repo.get_contributors()
+		for c in contributors:
+			print(c.name)
+
+	print("")
+	user_name = input("Pick a user: ")
+
+	if user_name == "quit":
+		break
+
+	match = False
+	selected_user = None
+	for user in contributors:
+		if user.name == user_name:
+			selected_user = user
+			match = True
+			break
+
+	if match == False:
+		print("That is not the name of any user.")
+
+	else:
+		repos = selected_user.get_repos()
+		for repo in repos:
+			print(repo.name)
